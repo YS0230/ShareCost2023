fa626f411ad32960584c6a7e1eda49f16db9d190
Chris <jiulrulsmu@gmail.com>
2023-05-21 22:59:13 +0800

    init db

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from dbModel import *
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
