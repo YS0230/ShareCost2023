20ab97d3e90949020c33d78f39d1c98fa5435dfc
Chris <jiulrulsmu@gmail.com>
2023-05-21 23:36:45 +0800

    add front

---
+++ front/build/precache-manifest.dae79d04cceeef78b86d3d2268518f65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f7b821abb70d2393f223068274c378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf1fe89a8337c83eb5a",
+    "url": "/static/css/2.28fd1748.chunk.css"
+  },
+  {
+    "revision": "63a54c94f3e437f78c10",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "fcf1fe89a8337c83eb5a",
+    "url": "/static/js/2.e144d219.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.e144d219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a54c94f3e437f78c10",
+    "url": "/static/js/main.e897c700.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
